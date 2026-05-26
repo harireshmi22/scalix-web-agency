b624fa6adcfe22e0f788b993c0ffd2278f1201ca
thalaforareasoncodes <astronautgamer2013@gmail.com>
2026-05-26 10:49:34 +0530

    Wrap children in LenisProvider in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       lang="en"
       
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+  <LenisProvider>{children}</LenisProvider>
+</body>
     </html>
   );
 }

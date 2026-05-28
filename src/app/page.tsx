4a2117140c1e57aa7d384b1fda00a707e957848f
harireshmi22 <harireshmi22@gmail.com>
2026-05-28 09:22:01 +0530

    feat: restructure homepage and integrate all agency sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,30 @@ import Service from "@/components/Service";
 import ChooseUs from "@/components/ChooseUs";
 import Footer from "@/components/Footer";
 import TeamMembers from "@/components/TeamMembers";
-
+import Header from "@/components/Header";
+import ProjectSwipper from "@/components/ProjectSwipper";
+import OurService from "@/components/OurService";
+import OurProject from "@/components/OurProject";
+import FAQ from "@/components/FAQ";
+import Banner from "@/components/Banner";
+import About from "@/components/About"
 
 export default function HomePage() {
   return (
     <main className="flex flex-col">
+      <Header />
       <Home />
+      <ProjectSwipper />
       <Services />
       <Service />
-      <div>
-        <Solutions />
-      </div>
-
-      <div>
-        <ChooseUs />
-      </div>
-
+      <Solutions />
+      <ChooseUs />
+      <OurProject />
+      <OurService />
       <TeamMembers />
-
+      <Banner />
+      <About />
+      <FAQ />
       <Footer />
     </main>
 

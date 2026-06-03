0d8fdd89b8267c7d854d1df4b9e2fdf550c7b27c
harireshmi22 <harireshmi22@gmail.com>
2026-06-03 12:28:49 +0530

    fix: add display swap option to font exports for better rendering

--- src/lib/fonts.js
+++ src/lib/fonts.js
@@ -1,6 +1,6 @@
 // src/lib/fonts.js
 import { Anton, Architects_Daughter, Geologica } from 'next/font/google';
 
-export const anton = Anton({ subsets: ["latin"], weight: "400" });
-export const architectsDaughter = Architects_Daughter({ subsets: ["latin"], weight: "400" });
-export const geologica = Geologica({ subsets: ["latin"], weight: ["300", "400"] });
+export const anton = Anton({ subsets: ["latin"], weight: "400", display: 'swap' });
+export const architectsDaughter = Architects_Daughter({ subsets: ["latin"], weight: "400", display: 'swap' });
+export const geologica = Geologica({ subsets: ["latin"], weight: ["300", "400"], display: 'swap' });

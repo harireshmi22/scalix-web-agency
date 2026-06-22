4732c861c4de62037496b98043925f20158491c6
harireshmi22 <harireshmi22@gmail.com>
2026-06-22 10:35:33 +0530

    feat: initialize homepage layout with core section components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,24 @@
 import Home from "@/components/Home";
-import Services from "@/components/Services";
-import Solutions from "@/components/Solutions";
-import Service from "@/components/Service";
-import ChooseUs from "@/components/ChooseUs";
 import TeamMembers from "@/components/TeamMembers";
 import ProjectSwipper from "@/components/ProjectSwipperClient";
 import OurService from "@/components/OurService";
 import OurProject from "@/components/OurProject";
 import FAQ from "@/components/FAQ";
 import Banner from "@/components/Banner";
-import About from "@/components/About"
+import About from "@/components/About";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col">
       <Home />
       <ProjectSwipper />
-      <Services />
-      <Service />
-      <Solutions />
-      <ChooseUs />
-      <OurProject />
-      <OurService />
-      <TeamMembers />
-      <Banner />
       <About />
+      <OurService />
+      <OurProject />
+      <TeamMembers />
       <FAQ />
+      <Banner />
     </div>
-
-
   );
 }
+
